ee026cc4b9703b87118dac3b2c962c515c25ab56
Xiamengs <akmalfazli27@gmail.com>
2026-01-23 16:59:24 +0700

    Add title and desctiption into the app

--- app/main.py
+++ app/main.py
@@ -5,10 +5,21 @@ import pandas as pd
 def main():
     st.set_page_config(
         page_title="Breast Cancer Predictor",
-        page_icon=":female-doctor:"
+        page_icon=":female-doctor:",
+        layout="wide",
+        initial_sidebar_state="expanded"
     )
 
-    st.write("hello world")
+    with st.container():
+        st.title("Breast Cancer Predictor")
+        st.write(
+            """
+            This app predicts whether a breast mass is **Benign** or **Malignant** 
+            based on measurements from a digitized image of a fine needle aspirate (FNA) 
+            of a breast mass. Update the measurements using the sliders in the sidebar 
+            to get a prediction.
+            """)
+
 
 if __name__ == '__main__':
     main()
