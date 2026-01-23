41d27986ec10fb7cae08b1f11e7d111bc473934d
Xiamengs <akmalfazli27@gmail.com>
2026-01-23 16:31:21 +0700

    Commit main.py for dashboard

---
+++ app/main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pickle as pickle
+import pandas as pd
+
+def main():
+    st.set_page_config(
+        page_title="Breast Cancer Predictor",
+        page_icon=":female-doctor:"
+    )
+
+    st.write("hello world")
+
+if __name__ == '__main__':
+    main()
+
